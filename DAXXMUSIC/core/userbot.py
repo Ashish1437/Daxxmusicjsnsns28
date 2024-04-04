268d96c0a2c9c33d3512920cfa4c99946d6b8228
DREAMERS-BOTS <159102147+DREAMERS-BOTS@users.noreply.github.com>
2024-04-04 17:11:22 +0530

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ALLTYPECC")
-                await self.one.join_chat("DAXXSUPPORT")
+                await self.one.join_chat("DREAMERS-BOTS")
+                await self.one.join_chat("Chat-Dreamer-bots")
             except:
                 pass
             assistants.append(1)
