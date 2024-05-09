f0414567a951a6df5fb27d3ba40ab39a52669a22
Ashish1437 <121593481+Ashish1437@users.noreply.github.com>
2024-05-09 14:10:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DREAMERS-BOTS/MUSICBOT",
+    "https://github.com/Ashish1437/Daxxmusicjsnsns28",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
